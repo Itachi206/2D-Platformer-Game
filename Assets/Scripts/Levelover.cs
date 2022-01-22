d3790568277d7c2607de9541c04bc690fea7e474
Yogesh Sonawane <96642408+Itachi206@users.noreply.github.com>
2022-01-22 17:02:13 +0530

    Added empty game object for level complete

---
+++ Assets/Scripts/Levelover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levelover : MonoBehaviour
+{
+    private void onTriggerEnter2D(Collision2D collision){
+        if(collision.gameObject.GetComponent<PlayerController>() != null){
+            Debug.Log("Level is finished");
+        }
+    }
+}
