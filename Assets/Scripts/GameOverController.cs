a331f4621c1b65f47470e118755c9e7a28f0ff31
Yogesh Sonawane <96642408+Itachi206@users.noreply.github.com>
2022-01-30 11:47:16 +0530

    Added Restart Button after Player death and game over controller script

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour
+{
+    public Button ButtonRestart;
+
+    private void Awake()
+    {
+        ButtonRestart.onClick.AddListener(ReloadLevel);
+    }
+    public void PlayerDied()
+    {
+        gameObject.SetActive(true);
+    }
+    private void ReloadLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
